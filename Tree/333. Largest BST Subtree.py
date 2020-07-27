faab87e404fc2aaf82cd00c919138975d56bf3e9
Junyi Zheng <zheng.junyi96@gmail.com>
2020-07-27 09:10:17 -0400

    Update 333. Largest BST Subtree.py

--- Tree/333. Largest BST Subtree.py
+++ Tree/333. Largest BST Subtree.py
@@ -8,6 +8,8 @@
 
 class Solution:
     def largestBSTSubtree(self, root: TreeNode) -> int:
+        # My method:
+
         # if a subtree is a BST  -> all of its subtree have
         #     left child < root < right child
         # because we need the result of its left and right subtree to judge if it is a BST
@@ -30,22 +32,27 @@ class Solution:
         #   else -> return -1
         # if leaf -> return 1
 
-        res = [0]
-        self.dfs(root, res)
-        return res[0]
+        # Leetcode discussion (best solution)
+        #   dfs() return 3 arugments,
+        # number of nodes in this subtree
+        # the minimum value in this subtree
+        # the maximum value in this subtree
+        #
+        #   The most tracky part is that, when reach the leaf nodes, we need to return  \
+        # number = 0, min = float('inf'), max = -float('inf').
+        # Because when we judge if the parent of the leaf nodes are BST, we need maximum of left and minimum of right. and the left nodes should always be assumed as a one node BST. Thus, -inf < root.val < inf is always valid.
+        # In addition, when any one of child is invaild. r_min = -inf, l_max = inf will make inf < root.val < -inf invalid.
+        #                       |
+        #          |---------|     | ---------|
 
-    def dfs(self, root, res):
-        # return left number
+        return self.dfs(root)[0]
+
+    def dfs(self, root):
         if not root:
-            return True, 0, float('INFINITY'), -float('INFINITY')
+            return 0, float('INFINITY'), -float('INFINITY')
 
-        l_bst, l_nodes, l_min, l_max = self.dfs(root.left, res)
-        r_bst, r_nodes, r_min, r_max = self.dfs(root.right, res)
-        sum_nodes = l_nodes + r_nodes + 1
-        root_bst = False
-        if l_max < root.val <= r_min:
-            root_bst = l_bst and r_bst
-        if root_bst:
-            prev_max = res.pop()
-            res.append(max(prev_max, sum_nodes))
-        return root_bst, sum_nodes, min(l_min, r_min, root.val), max(r_max, l_max, root.val)
+        l_nodes, l_min, l_max = self.dfs(root.left)
+        r_nodes, r_min, r_max = self.dfs(root.right)
+        if l_max < root.val < r_min:
+            return l_nodes + r_nodes + 1, min(l_min, root.val), max(root.val, r_max)
+        return max(l_nodes, r_nodes), -float('inf'), float('inf')
