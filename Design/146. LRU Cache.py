f6bfa2e927f8c7afe41e2340a91ed5f7f57881e0
Junyi Zheng <zheng.junyi96@gmail.com>
2020-07-29 20:57:42 -0400

    Update 146. LRU Cache.py

--- Design/146. LRU Cache.py
+++ Design/146. LRU Cache.py
@@ -1,3 +1,8 @@
+'''
+use ordered dictionary
+'''
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -28,3 +33,65 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+
+'''
+use double linked list + dictionary
+'''
+
+
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    # use double linked list + dictionary
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dic = {}  # key -> node
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+        node = self.dic[key]
+        self.remove_node(node)
+        self.add_node(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            self.remove_node(self.dic[key])
+        new_node = Node(key, value)
+        self.add_node(new_node)
+        self.dic[key] = new_node
+        if len(self.dic) > self.capacity:
+            removed_node = self.head.next
+            self.remove_node(removed_node)
+            self.dic.pop(removed_node.key)
+
+    def remove_node(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def add_node(self, node):
+        last_node = self.tail.prev
+        last_node.next = node
+        node.prev = last_node
+        node.next = self.tail
+        self.tail.prev = node
+
+    # Your LRUCache object will be instantiated and called as such:
+    # obj = LRUCache(capacity)
+    # param_1 = obj.get(key)
+    # obj.put(key,value)
