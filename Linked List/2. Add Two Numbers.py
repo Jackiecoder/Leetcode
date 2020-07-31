b2360ea68c3658cc529ab45905364224c41692e9
Junyi Zheng <zheng.junyi96@gmail.com>
2020-07-31 10:22:27 -0400

    Update 2. Add Two Numbers.py

--- Linked List/2. Add Two Numbers.py
+++ Linked List/2. Add Two Numbers.py
@@ -7,31 +7,27 @@
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode(0)
+        cur = dummy
         carry = 0
-        dummynode = ListNode(0)
-        cur = dummynode
         while l1 and l2:
-            sumup = l1.val + l2.val + carry
-            number = sumup % 10
-            carry = sumup // 10
-            cur.next = ListNode(number)
+            ssum = l1.val + l2.val + carry
+            cur.next = ListNode(ssum % 10)
+            carry = ssum // 10
+            cur = cur.next
             l1 = l1.next
             l2 = l2.next
-            cur = cur.next
-        while l1:
-            sumup = l1.val + carry
-            number = sumup % 10
-            carry = sumup // 10
-            cur.next = ListNode(number)
-            l1 = l1.next
-            cur = cur.next
-        while l2:
-            sumup = l2.val + carry
-            number = sumup % 10
-            carry = sumup // 10
-            cur.next = ListNode(number)
-            l2 = l2.next
+        remain = None
+        if l1:
+            remain = l1
+        if l2:
+            remain = l2
+        while remain:
+            ssum = remain.val + carry
+            cur.next = ListNode(ssum % 10)
+            carry = ssum // 10
+            remain = remain.next
             cur = cur.next
         if carry:
             cur.next = ListNode(carry)
-        return dummynode.next
+        return dummy.next
