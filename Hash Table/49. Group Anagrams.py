38674156662ad4b459f76b0a94ad529a6d70586a
Junyi Zheng <zheng.junyi96@gmail.com>
2020-08-09 10:18:29 -0400

    Update 49. Group Anagrams.py

--- Hash Table/49. Group Anagrams.py
+++ Hash Table/49. Group Anagrams.py
@@ -1,5 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # instead of sort each string, just count the frequency of each char, and make a array
+        # e.g. 'eat' -> 100010000000....1000000
+        # Time O(n + k), space O(26n) = O(n)
+        hash_to_words = defaultdict(list)
+        for string in strs:
+            count_char = [0] * 26
+            for char in string:
+                count_char[ord(char) - ord('a')] += 1
+            string_freq = ''
+            for count in count_char:
+                string_freq += str(count)
+            hash_to_words[string_freq].append(string)
+        return [val for val in hash_to_words.values()]
+
+    def groupAnagrams_sortString(self, strs: List[str]) -> List[List[str]]:
         # hash table = { hash table = { char: times } : [eat, tea, ...]}
         # Time O(n * klogk), space O(n)
         hash_to_words = defaultdict(list)
