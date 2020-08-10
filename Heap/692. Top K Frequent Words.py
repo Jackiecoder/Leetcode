1cb3c74b0a798497093dfef983eccb7b2cedad7e
Junyi Zheng <zheng.junyi96@gmail.com>
2020-08-10 13:57:59 -0400

    Update 692. Top K Frequent Words.py

--- Heap/692. Top K Frequent Words.py
+++ Heap/692. Top K Frequent Words.py
@@ -1,12 +1,43 @@
+class Word:
+    def __init__(self, word, freq):
+        self.word = word
+        self.freq = freq
+
+    def __lt__(self, other):
+        if self.freq == other.freq:
+            return self.word > other.word
+        return self.freq < other.freq
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        # we want high frequency and low word order
-        word_count = collections.Counter(words)
-        heap = [(-count, word) for word, count in word_count.items()]
-        heapq.heapify(heap)
+        # target: O(nlogk)
+        # maintain a size k heap
+        # heap = [(freq, string)] so the heap will pop out smallest freq, and largest string first.
+        # so peak of heap is the tuple with (small freq, and large string)
+        # then in the heap, just maintain a size k heap
+        counter = Counter(words)
+        heap = []
+        for word, freq in counter.items():
+            heapq.heappush(heap, Word(word, freq))
+            if len(heap) > k:
+                heapq.heappop(heap)
         res = []
-        for _ in range(k):
-            if not heap:
-                break
-            res.append(heapq.heappop(heap)[1])
+        while heap:
+            word = heapq.heappop(heap)
+            res.append(word.word)
+        return res[::-1]
+
+    def topKFrequent_klogn(self, words: List[str], k: int) -> List[str]:
+        # hash table = {word: freq}
+        # then use priority queue to find top k freq
+        # -> implement: heapify (-freq, string), pop k times
+        # time O(n + klogn), space O(n)
+        counter = Counter(words)
+        heap = [(-freq, word) for word, freq in counter.items()]
+        heapq.heapify(heap)  # O(n)
+        res = []
+        for _ in range(k):                # O(k)
+            _, word = heapq.heappop(heap)  # O(logn)
+            res.append(word)
         return res
