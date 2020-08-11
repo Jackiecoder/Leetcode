244ffa08f124e1c2472baf29d0929aaf9fb45bf8
Junyi Zheng <zheng.junyi96@gmail.com>
2020-08-11 16:42:37 -0400

    Update 146. LRU Cache.py

--- Design/146. LRU Cache.py
+++ Design/146. LRU Cache.py
@@ -1,97 +1,69 @@
-'''
-use ordered dictionary
-'''
-
-
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.remain = capacity
-        self.dic = collections.OrderedDict()
-
-    def get(self, key: int) -> int:
-        if key in self.dic:
-            ret = self.dic[key]
-            self.dic.pop(key)
-            self.dic[key] = ret
-        else:
-            ret = -1
-        return ret
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            self.dic.pop(key)
-            self.dic[key] = value
-        else:
-            if self.remain == 0:
-                self.dic.popitem(last=False)
-                self.remain += 1
-            self.dic[key] = value
-            self.remain -= 1
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
-
-
-'''
-use double linked list + dictionary
-'''
-
-
 class Node:
     def __init__(self, key, val):
-        self.key = key
         self.val = val
-        self.prev = None
         self.next = None
+        self.prev = None
+        self.key = key
 
 
 class LRUCache:
-    # use double linked list + dictionary
+
+    '''
+    dic = {key1: (0, val1)} 
+    list = [key1, key2]
+    capacity = 2
+    1. Time:
+        get() -> O(n)
+        put() -> O(n)
+
+    -- Follow Up ---
+    dic = {key1: node1}
+    node1(key1, val) -> node2(key2, val) -> node3(key3, val)
+    '''
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.dic = {}  # key -> node
         self.head = Node(0, 0)
         self.tail = Node(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
+        self.dic = {}  # key : node
+        self.capacity = capacity
 
-    def get(self, key: int) -> int:
-        if key not in self.dic:
-            return -1
-        node = self.dic[key]
-        self.remove_node(node)
-        self.add_node(node)
-        return node.val
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            self.remove_node(self.dic[key])
-        new_node = Node(key, value)
-        self.add_node(new_node)
-        self.dic[key] = new_node
-        if len(self.dic) > self.capacity:
-            removed_node = self.head.next
-            self.remove_node(removed_node)
-            self.dic.pop(removed_node.key)
-
-    def remove_node(self, node):
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
-
-    def add_node(self, node):
+    def _add(self, node):
+        # add to tail
         last_node = self.tail.prev
         last_node.next = node
         node.prev = last_node
         node.next = self.tail
         self.tail.prev = node
 
-    # Your LRUCache object will be instantiated and called as such:
-    # obj = LRUCache(capacity)
-    # param_1 = obj.get(key)
-    # obj.put(key,value)
+    def _remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+        node = self.dic[key]
+        self._remove(node)
+        self._add(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            self._remove(self.dic[key])
+        node = Node(key, value)
+        self.dic[key] = node
+        self._add(node)
+        if len(self.dic) > self.capacity:
+            del_node = self.head.next
+            self._remove(del_node)
+            self.dic.pop(del_node.key)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
