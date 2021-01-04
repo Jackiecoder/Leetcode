e12753e214c4bf9b6bd32fc610703480b8d70cf2
Junyi Zheng <zheng.junyi96@gmail.com>
2021-01-04 16:17:53 -0500

    Update 236. Lowest Common Ancestor of a Binary Tree.py

--- Python_codeWzLily/236. Lowest Common Ancestor of a Binary Tree.py
+++ Python_codeWzLily/236. Lowest Common Ancestor of a Binary Tree.py
@@ -17,19 +17,40 @@ class Solution:
         #       1. root.left and root.right contain one node
         #       2. root.left contain two
         #       3. root.left contain one, and node is another one
-        node, _ = self.dfs(root, p, q, 0)
-        return node
 
-    def dfs(self, node, p, q, found):
+        # method 1
+        # node, _ = self.dfs1(root, p, q, 0)
+        # return node
+
+        # method 2
+        return self.dfs(root, p, q)
+
+    def dfs(self, node, p, q
+        # because this node must exist, so once we find a valid node, we can return this node immediately.
+        if not node:
+            return None
+        if node is p or node is q:
+            return node
+        left = self.dfs(node.left, p, q)
+        right = self.dfs(node.right, p, q)
+        if left and right:
+            return node
+        if left:
+            return left
+        if right:
+            return right
+
+
+    def dfs1(self, node, p, q, found):
         if not node:
             return None, 0
-        left, left_found = self.dfs(node.left, p, q, found)
-        right, right_found = self.dfs(node.right, p, q, found)
+        left, left_found=self.dfs(node.left, p, q, found)
+        right, right_found=self.dfs(node.right, p, q, found)
         if left_found == 2:
             return left, 2
         if right_found == 2:
             return right, 2
-        found = 1 if node is p or node is q else 0
+        found=1 if node is p or node is q else 0
         found += left_found + right_found
         if found == 2:
             return node, found
