3c568a3fa8d59ce3d58501be41cbe312ef07a6b0
Junyi Zheng <zheng.junyi96@gmail.com>
2021-01-21 12:27:30 -0500

    Update 75. Sort Colors.py

--- Python_codeWzLily/75. Sort Colors.py
+++ Python_codeWzLily/75. Sort Colors.py
@@ -1,5 +1,27 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
+        # quick sort
+        # Time O(n * log3), space O(1)
+        self.quickSort(nums, 0, len(nums) - 1, 0, 2)
+
+    def quickSort(self, nums, l, r, min, max):
+        if l >= r or min > max:
+            return
+        mid = (min + max) // 2 + (min + max) % 2
+        p = self.partition(nums, l, r, mid)
+        self.quickSort(nums, l, p - 1, min, mid - 1)
+        self.quickSort(nums, p, r, mid + 1, max)
+
+    def partition(self, nums, l, r, pivot):
+        greater = l
+        # print(nums, l, r, pivot)
+        for i in range(l, r + 1):
+            if nums[i] < pivot:
+                nums[i], nums[greater] = nums[greater], nums[i]
+                greater += 1
+        return greater
+
+    def sortColors_twoPointer(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
